d3cb7d68aa5baad5e64f6719ad11c6efded2e9cb
Walter Indeche <walter.indeche@andela.com>
2021-06-17 11:29:21 +0300

    Created a service to return jokes

---
+++ src/main/java/com/walter/springbootjokesapp/service/JokesService.java
@@ -0,0 +1,5 @@
+package com.walter.springbootjokesapp.service;
+
+public interface JokesService {
+    String getJoke();
+}
---
+++ src/main/java/com/walter/springbootjokesapp/service/JokesServiceImpl.java
@@ -0,0 +1,18 @@
+package com.walter.springbootjokesapp.service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokesServiceImpl  implements JokesService{
+    private  final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
