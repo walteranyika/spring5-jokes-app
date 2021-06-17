3cb6b9b79c8c16885c320773de01e48a1f3e9512
Walter Indeche <walter.indeche@andela.com>
2021-06-17 11:17:43 +0300

    Spring 5 Jokes App

---
+++ src/main/java/com/walter/springbootjokesapp/SpringBootJokesAppApplication.java
@@ -0,0 +1,13 @@
+package com.walter.springbootjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJokesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJokesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/walter/springbootjokesapp/SpringBootJokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.springbootjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
